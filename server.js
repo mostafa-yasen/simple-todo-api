76201ba86085dee078dab304a3608037e18dac56
Mostafa Yasin <mostafa.a.yasin@gmail.com>
2021-11-14 00:13:56 +0200

    setting up database connection

---
+++ server.js
@@ -0,0 +1,23 @@
+const PORT = 9090
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+
+
+
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+
+db.on('error', (err) => {
+    console.error(err)
+})
+
+db.once('open', () => {
+    console.log('Connected to Database')
+})
+
+const app = express()
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on http://localhost:${PORT}`)
+})
