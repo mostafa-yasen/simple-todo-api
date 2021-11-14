6f55cd253b069e0b9431fcf4148e29177682cdc6
Mostafa Yasin <mostafa.a.yasin@gmail.com>
2021-11-14 02:00:37 +0200

    minor fix in response model

--- models/response.js
+++ models/response.js
@@ -1,8 +1,8 @@
 
-exports.Respons = class Response {
-    constructor(code=200, error_code=null, data=null, message=null, _message=null) {
-        this.code = code || null
-        this.error_code = error_code || null
+exports.ApiResponse = class ApiResponse {
+    constructor(code, errorCode, data, message, _message) {
+        this.code = code || 200
+        this.errorCode = errorCode || null
         this.data = data || null
         this.message = message || null
         this._message = _message || null
